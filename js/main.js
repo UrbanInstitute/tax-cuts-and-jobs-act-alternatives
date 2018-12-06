4de4fb423cab00171e5ea05cbf52e91f2548d768
Ben Chartoff <bchartoff@gmail.com>
2018-12-06 09:39:45 -0500

    add filter checkboxes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 d3.json("data/data.json", function(points){
+  var shown;
+
   var margin = {top: 20, right: 10, bottom: 30, left: 40},
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
@@ -9,11 +11,11 @@ d3.json("data/data.json", function(points){
     .append("g")
       .attr("transform", "translate(" + margin.left + " " + margin.top + ")");
 
-  var factory = d3.quadtree()
-    .extent([
-      [0, 0],
-      [width, height]
-    ]);
+  // var factory = d3.quadtree()
+  //   .extent([
+  //     [0, 0],
+  //     [width, height]
+  //   ]);
 
   var x = d3.scaleLinear()
     .range([0, width]);
@@ -36,7 +38,9 @@ d3.json("data/data.json", function(points){
 
   var chartArea = d3.select("#chartContainer").append("div")
     .style("left", margin.left + "px")
-    .style("top", margin.top + "px");
+    .style("top", margin.top + "px")
+    .style("position", "absolute")
+    // .style("z-index","-1");
 
   var canvas = chartArea.append("canvas")
     .attr("width", width)
@@ -44,21 +48,25 @@ d3.json("data/data.json", function(points){
 
   var context = canvas.node().getContext("2d");
 
-  context.fillStyle = "rgba(0, 0, 0, 0.2)";
+  
 
   // Layer on top of canvas, example of selection details
   var highlight = chartArea.append("svg")
     .attr("width", width)
     .attr("height", height)
+    .style("position", "absolute")
+    .style("left", 0)
+    .style("top", 0)
       .append("circle")
-        .attr("r", 7)
+        .attr("r", 3)
         .classed("hidden", true);
 
   function init(){
     points.forEach(function(p,i){
-      p.x = p["inc_all_all"]
+      p.x = p["inc_q1_all"]
       p.y = p["burden"]
     })
+    shown = points.map(function(p){ return [p.x, p.y] })
 
   }
 
@@ -78,6 +86,14 @@ d3.json("data/data.json", function(points){
     yg.call(yAxis);
 
 
+    var tree = d3.quadtree()
+    .extent([
+      [0, 0],
+      [width, height]
+    ])
+
+
+
     // Update canvas
     context.clearRect(0, 0, width, height);
 
@@ -85,25 +101,81 @@ d3.json("data/data.json", function(points){
 
       context.beginPath();
       context.arc(x(p.x), y(p.y), 3, 0, 2 * Math.PI);
-      context.fill();
-
+      if(p.hide){
+         context.fillStyle = "rgba(0, 0, 0, 0.008)";
+         context.fill();
+      }
     });
+
+    points.forEach(function(p,i){
+
+      context.beginPath();
+      context.arc(x(p.x), y(p.y), 3, 0, 2 * Math.PI);
+      if(!p.hide){
+         context.fillStyle = "rgba(22,150,210, 0.3)";
+         context.fill();
+         tree.add([p.x, p.y])
+      }
+    });
+
+    console.log(tree.data())
+    console.log(shown)
+
+
+// var  shown2 = points.filter(function(p){ return p.hide == false })
+//     .map(function(p){ return [p.x, p.y] })
+
+
+    // .addAll(shown2);
+
+  canvas.on("mousemove",function(){
+
+    var mouse = d3.mouse(this),
+        closest = tree.find(x.invert(mouse[0]), y.invert(mouse[1]),1);
+    // tree.visit(function(node, x0, y0, x1, y1){
+    //   // console.log(x0, y0)
+    // })
+    // console.log(mouse, closest)
+
+    if(typeof(closest) != "undefined"){
+      highlight.classed("hidden", false)
+        .attr("cx", x(closest[0]))
+        .attr("cy", y(closest[1]));
+    }else{
+      highlight.classed("hidden", true);
+    }
+
+  });
+
+  canvas.on("mouseover",function(){
+    highlight.classed("hidden", false);
+  });
+
+  canvas.on("mouseout",function(){
+    highlight.classed("hidden", true);
+  });
+
 
   }
 
   init();
 
+
   draw(); 
 
 d3.select("#groupMenu").on("input", function(){
-  // console.log(this.value)
   animateLayout(d3.select("#incomeMenu").node().value, this.value)
 })
 
 d3.select("#incomeMenu").on("input", function(){
-  // console.log(this.value)
   animateLayout(this.value, d3.select("#groupMenu").node().value)
 })
+
+
+d3.selectAll(".control")
+  .on("input", function(){
+    filterPoints(getFilterVals())
+  })
 
 
 
@@ -111,8 +183,44 @@ const duration = 1000;
 const ease = d3.easeCubic;
 let timer;
 
+function getFilterVals(){
+  var returned = {}
+  var filterVars = ["standard", "rates", "amtThreshold", "amtAmount", "personal","salt", "ctcAmount", "ctcThreshold"]
+  for(var i = 0; i < filterVars.length; i++){
+    var vals = []
+    var filterVar = filterVars[i]
+    d3.selectAll(".control." + filterVar).each(function(){
+      if(this.checked){ vals.push(this.value) }
+    })
+    returned[filterVar] = vals
+  }
+  return returned
+}
 
+function filterPoints(filters){
+  points.forEach(function(point){
+    point.hide = false;
+    for(var filter in filters){
+      if(point.hide){
+        continue
+      }else{
+        if(filters.hasOwnProperty(filter)){
+          var vals = filters[filter]
+          if(vals.indexOf(point[filter]) == -1){
+              point.hide = true
+          }else{
+            point.hide = false;
+          }
+        }
+      }
+    }
 
+  })
+  shown = points.filter(function(p){ return p.hide == false })
+    .map(function(p){ return [p.x, p.y] })
+  draw();
+
+}
 
 function animateLayout(income, group) {
   // store the source position
@@ -123,6 +231,8 @@ function animateLayout(income, group) {
   points.forEach(function(point){
     point.tx = point["inc_" + income + "_" + group];
   });
+
+  
 
   timer = d3.timer(function(elapsed){
     // compute how far through the animation we are (0 to 1)
@@ -139,6 +249,9 @@ function animateLayout(income, group) {
     // if this animation is over
     if (t === 1) {
       // stop this timer for this layout and start a new one
+      console.log("foo")
+      shown = points.map(function(p){ return [p.x, p.y] })
+      draw();
       timer.stop()
 
     }
@@ -146,28 +259,5 @@ function animateLayout(income, group) {
 }
 
 
-  function getPoints(income, group){
-    
-      // console.log(data)
-      return data.map(function(d){
-        return [
-          d["inc_" + income + "_" + group],
-          d["burden"]
-        ]
-      })
-  }
 
-  function randomPoints(scale) {
-
-    // Get points
-    return d3.range(1000).map(function(d){
-
-      return [
-        Math.random() * scale,
-        Math.random() * scale
-      ];
-
-    });
-
-  }
 })
--- reshape.py
+++ reshape.py
@@ -9,12 +9,104 @@ for i in range(0, len(head)):
 	h[head[i]] = i
 
 byParam = {}
+count = 0
 for row in cr:
-	param = row[h["paramfilename"]]
+	
+	# TO BE REMOVED, DEBUGGER
+	# param = 
+
+	param = row[h["paramfilename"]]	
 	if param not in byParam:
 		byParam[param] = {}
 
+	
+
 	byParam[param]["burden"] = float(row[h["burden_chg"]])
+	standard = float(row[h["standard1"]])
+	if standard == 6500:
+		byParam[param]["standard"] = "low"
+	elif standard == 9250:
+		byParam[param]["standard"] = "mediumLow"
+	elif standard == 12000:
+		byParam[param]["standard"] = "mediumHigh"
+	elif standard == 13200:
+		byParam[param]["standard"] = "high"
+	else:
+		print "uncategorized rate"
+
+# pretty brittle, but 4 rate structures all have different vals for 2nd bracket, so for now this is working
+	rates = float(row[h["rates2"]])
+	if rates == .15:
+		byParam[param]["rates"] = "a"
+	elif rates == .12:
+		byParam[param]["rates"] = "b"
+	elif rates == .13:
+		byParam[param]["rates"] = "c"
+	elif rates == .14:
+		byParam[param]["rates"] = "d"
+	else:
+		print "uncategoried rate"
+
+	amtThreshold = float(row[h["amthrsh1"]])
+	if amtThreshold == 123300:
+		byParam[param]["amtThreshold"] = "low"
+	elif amtThreshold == 500000:
+		byParam[param]["amtThreshold"] = "high"
+	else:
+		print "uncategorized amt threshold"
+
+	amtAmount = float(row[h["amtx1"]])
+	if amtAmount == 55400:
+		byParam[param]["amtAmount"] = "low"
+	elif amtAmount == 70300:
+		byParam[param]["amtAmount"] = "high"
+	else:
+		print "uncategorized amt amount"
+
+	personal = float(row[h["exemption_amount"]])
+	if personal == 0:
+		byParam[param]["personal"] = "low"
+	elif personal == 2050:
+		byParam[param]["personal"] = "mediumLow"
+	elif personal == 4150:
+		byParam[param]["personal"] = "mediumHigh"
+	elif personal == 5500:
+		byParam[param]["personal"] = "high"
+	else:
+		print "uncategoried personal exemption"
+
+	salt = float(row[h["saltcap"]])
+	if salt == 0:
+		byParam[param]["salt"] = "low"
+	elif salt == 10000:
+		byParam[param]["salt"] = "mediumLow"
+	elif salt == 15000:
+		byParam[param]["salt"] = "mediumHigh"
+	elif salt == 20000:
+		byParam[param]["salt"] = "high"
+	else:
+		print "uncategoried salt tax"
+
+	ctcThreshold = float(row[h["childrefund_thresh"]])
+	if ctcThreshold == 0:
+		byParam[param]["ctcThreshold"] = "low"
+	elif ctcThreshold == 1250:
+		byParam[param]["ctcThreshold"] = "medium"
+	elif ctcThreshold == 2500:
+		byParam[param]["ctcThreshold"] = "high"
+	else:
+		print "uncategoried ctc threshold"
+
+	ctcAmount = float(row[h["ctc_refund_amount"]])
+	if ctcAmount == 1000:
+		byParam[param]["ctcAmount"] = "low"
+	elif ctcAmount == 1400:
+		byParam[param]["ctcAmount"] = "medium"
+	elif ctcAmount == 2000:
+		byParam[param]["ctcAmount"] = "high"
+	else:
+		print "uncategoried ctc amount"
+
 
 	categories = ["all","elderly","hoh","kids","mfj","mfs","mkids","mminorkids","myoungkids","nokids","notelderly","single"]
 	for category in categories:
@@ -32,6 +124,7 @@ for row in cr:
 
 dataOut = []
 for d in byParam:
+# 	if(d.find("batch_1") != -1):
 	dataOut.append(byParam[d])
 
 with open('data/pretty.json', 'wt') as out:
