ae08cd99bd330a11cbdcc42363b4f2276ad529e4
Ben Chartoff <bchartoff@gmail.com>
2018-12-13 11:57:18 -0500

    more functional scroll skeleton, and b, and switch burden -> revenue

--- reshape.py
+++ reshape.py
@@ -25,16 +25,16 @@ for row in cr:
 	#####
 	
 
-	byParam[param]["burden"] = float(row[h["burden_chg"]])
+	byParam[param]["burden"] = float(row[h["rev_chg"]])
 	standard = float(row[h["standard1"]])
 	if standard == 6500:
-		byParam[param]["standard"] = "low"
+		byParam[param]["standard"] = "l"
 	elif standard == 9250:
-		byParam[param]["standard"] = "mediumLow"
+		byParam[param]["standard"] = "ml"
 	elif standard == 12000:
-		byParam[param]["standard"] = "mediumHigh"
+		byParam[param]["standard"] = "mh"
 	elif standard == 13200:
-		byParam[param]["standard"] = "high"
+		byParam[param]["standard"] = "h"
 	else:
 		print "uncategorized rate"
 
@@ -53,78 +53,79 @@ for row in cr:
 
 	amtThreshold = float(row[h["amthrsh1"]])
 	if amtThreshold == 123300:
-		byParam[param]["amtThreshold"] = "low"
+		byParam[param]["amtThreshold"] = "l"
 	elif amtThreshold == 500000:
-		byParam[param]["amtThreshold"] = "high"
+		byParam[param]["amtThreshold"] = "h"
 	else:
 		print "uncategorized amt threshold"
 
 	amtAmount = float(row[h["amtx1"]])
 	if amtAmount == 55400:
-		byParam[param]["amtAmount"] = "low"
+		byParam[param]["amtAmount"] = "l"
 	elif amtAmount == 70300:
-		byParam[param]["amtAmount"] = "high"
+		byParam[param]["amtAmount"] = "h"
 	else:
 		print "uncategorized amt amount"
 
 	personal = float(row[h["exemption_amount"]])
 	if personal == 0:
-		byParam[param]["personal"] = "low"
+		byParam[param]["personal"] = "l"
 	elif personal == 2050:
-		byParam[param]["personal"] = "mediumLow"
+		byParam[param]["personal"] = "ml"
 	elif personal == 4150:
-		byParam[param]["personal"] = "mediumHigh"
+		byParam[param]["personal"] = "mh"
 	elif personal == 5500:
-		byParam[param]["personal"] = "high"
+		byParam[param]["personal"] = "h"
 	else:
 		print "uncategoried personal exemption"
 
 	salt = float(row[h["saltcap"]])
 	if salt == 0:
-		byParam[param]["salt"] = "low"
+		byParam[param]["salt"] = "l"
 	elif salt == 10000:
-		byParam[param]["salt"] = "mediumLow"
+		byParam[param]["salt"] = "ml"
 	elif salt == 15000:
-		byParam[param]["salt"] = "mediumHigh"
+		byParam[param]["salt"] = "mh"
 	elif salt == 20000:
-		byParam[param]["salt"] = "high"
+		byParam[param]["salt"] = "h"
 	else:
 		print "uncategoried salt tax"
 
 	ctcThreshold = float(row[h["childrefund_thresh"]])
 	if ctcThreshold == 0:
-		byParam[param]["ctcThreshold"] = "low"
+		byParam[param]["ctcThreshold"] = "l"
 	elif ctcThreshold == 1250:
 		byParam[param]["ctcThreshold"] = "medium"
 	elif ctcThreshold == 2500:
-		byParam[param]["ctcThreshold"] = "high"
+		byParam[param]["ctcThreshold"] = "h"
 	else:
 		print "uncategoried ctc threshold"
 
 	ctcAmount = float(row[h["ctc_refund_amount"]])
 	if ctcAmount == 1000:
-		byParam[param]["ctcAmount"] = "low"
+		byParam[param]["ctcAmount"] = "l"
 	elif ctcAmount == 1400:
 		byParam[param]["ctcAmount"] = "medium"
 	elif ctcAmount == 2000:
-		byParam[param]["ctcAmount"] = "high"
+		byParam[param]["ctcAmount"] = "h"
 	else:
 		print "uncategoried ctc amount"
 
 
 	categories = ["all","elderly","hoh","kids","mfj","mfs","mkids","mminorkids","myoungkids","nokids","notelderly","single"]
+	categoryDict = {"all": "a","elderly":"b","hoh":"c","kids":"d","mfj":"e","mfs":"f","mkids":"g","mminorkids":"h","myoungkids":"i","nokids":"j","notelderly":"k","single":"l"}
 	for category in categories:
 		if(row[h["category"]] == category):
-			byParam[param]["inc_all_" + category] = float(row[h["pctchginafttaxincpercent_pct_12"]])
-			byParam[param]["inc_q1_" + category] = float(row[h["pctchginafttaxincpercent_pct_2"]])
-			byParam[param]["inc_q2_" + category] = float(row[h["pctchginafttaxincpercent_pct_3"]])
-			byParam[param]["inc_q3_" + category] = float(row[h["pctchginafttaxincpercent_pct_4"]])
-			byParam[param]["inc_q4_" + category] = float(row[h["pctchginafttaxincpercent_pct_5"]])
-			byParam[param]["inc_q5_" + category] = float(row[h["pctchginafttaxincpercent_pct_13"]])
-			byParam[param]["inc_top10_" + category] = float(row[h["pctchginafttaxincpercent_pct_14"]])
-			byParam[param]["inc_top5_" + category] = float(row[h["pctchginafttaxincpercent_pct_15"]])
-			byParam[param]["inc_top1_" + category] = float(row[h["pctchginafttaxincpercent_pct_16"]])
-			byParam[param]["inc_top05_" + category] = float(row[h["pctchginafttaxincpercent_pct_17"]])
+			byParam[param][categoryDict[category] + "0"] = float(row[h["pctchginafttaxincpercent_pct_12"]])
+			byParam[param][categoryDict[category] + "1"] = float(row[h["pctchginafttaxincpercent_pct_2"]])
+			byParam[param][categoryDict[category] + "2"] = float(row[h["pctchginafttaxincpercent_pct_3"]])
+			byParam[param][categoryDict[category] + "3"] = float(row[h["pctchginafttaxincpercent_pct_4"]])
+			byParam[param][categoryDict[category] + "4"] = float(row[h["pctchginafttaxincpercent_pct_5"]])
+			byParam[param][categoryDict[category] + "5"] = float(row[h["pctchginafttaxincpercent_pct_13"]])
+			byParam[param][categoryDict[category] + "6"] = float(row[h["pctchginafttaxincpercent_pct_14"]])
+			byParam[param][categoryDict[category] + "7"] = float(row[h["pctchginafttaxincpercent_pct_15"]])
+			byParam[param][categoryDict[category] + "8"] = float(row[h["pctchginafttaxincpercent_pct_16"]])
+			byParam[param][categoryDict[category] + "9"] = float(row[h["pctchginafttaxincpercent_pct_17"]])
 
 dataOut = []
 for d in byParam:
